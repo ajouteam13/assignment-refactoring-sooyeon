de57ef7d68258585383392b01c06eec7cc7d2aaa
sooyeon <tndus130@ajou.ac.kr>
2019-07-21 03:28:40 +0900

    upload files in dirt directory

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -9,54 +9,77 @@ class DirtySample {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            if (!equals_Name(items[i], "Aged Brie") && !equals_Name(items[i],"Backstage passes to a TAFKAL80ETC concert")) {
+                if (quality_more_than_0(items[i])) {
+                    if (!equals_Name(items[i],"Sulfuras, Hand of Ragnaros")) {
+                        quality_decrease(items[i]);
+                        sellin_decrease(items[i]);
                     }
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+            }
+            else {
+                if (quality_less_than_50(items[i])) {
+                    quality_increase(i); //age
+                    if (equals_Name(items[i],"Backstage passes to a TAFKAL80ETC concert")){ //back
+                        if (sellin_less_than(items[i].sellIn, 11)) {
+                                quality_increase(i);
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (sellin_less_than(items[i].sellIn, 6)) {
+                                quality_increase(i);
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (sellin_less_than(items[i].sellIn, 0)) {
+                if (!equals_Name(items[i], "Aged Brie")) {
+                    if (!equals_Name(items[i],"Backstage passes to a TAFKAL80ETC concert")) {
+                        if (quality_more_than_0(items[i])) {
+                            if (!equals_Name(items[i],"Sulfuras, Hand of Ragnaros")) {
+                                quality_decrease(items[i]);
+                                sellin_decrease(items[i]);
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    else { //back
+                        items[i].quality = 0;
+                    }
+                }
+                else { //age
+                    if (quality_less_than_50(items[i])) {
+                        quality_increase(i);
                     }
                 }
             }
         }
     }
+
+    private boolean sellin_less_than(int sellIn, int compare) {
+        return sellIn < compare;
+    }
+
+    private boolean quality_more_than_0(Item item) {
+        return item.quality > 0;
+    }
+
+    private boolean quality_less_than_50(Item item) {
+        return item.quality < 50;
+    }
+
+    private void sellin_decrease(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void quality_decrease(Item item) {
+        item.quality = item.quality - 1;
+    }
+
+    private void quality_increase(int i) {
+        items[i].quality = items[i].quality + 1;
+    }
+
+    private boolean equals_Name(Item item, String name) {
+        return item.name.equals(name);
+    }
+
 }
